9318761c1629b1a5eb6f02eaf8b557d8d61cec68
Noobie777 <42256429+Noobie777@users.noreply.github.com>
2020-05-29 20:32:03 +0530

    Add files via upload

---
+++ Escape Game/Libraries/execute.js
@@ -0,0 +1,15 @@
+function f1(){
+	alert("Hello")
+}
+function welcome(){
+	window.location.href = "Welcome.html"
+	// window.alert("hello")
+	// if (document.getElementById("username").value()=="abc" && document.getElementById("password").value()=="123") {
+	// 	window.location.href = "Welcome.html"
+	// else{
+	// 	window.alert("wrong")
+	// }
+}
+function assign(){
+	window.location.href = "Assign.html"
+}
