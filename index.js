9c57e78330e17f451d3308933671ac38b5ab7560
YokaiEmporer <taranrishit1234@gmail.com>
2020-06-04 00:51:34 +0530

    Updates

---
+++ index.js
@@ -0,0 +1,4 @@
+import React from "react"
+import ReactDom from "react-dom"
+import App from "./Components/App_test"
+ReactDom.render(<App />,document.getElementById("root")) 
---
+++ precache-manifest.abf363534860cc059628c6962c93fd00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310454687e43c7927b1234bdd4f771a4",
+    "url": "/escape-game/index.html"
+  },
+  {
+    "revision": "c8c8668b533ce46879d3",
+    "url": "/escape-game/static/css/main.7b71c78a.chunk.css"
+  },
+  {
+    "revision": "58120cfccb17f101a214",
+    "url": "/escape-game/static/js/2.7171896b.chunk.js"
+  },
+  {
+    "revision": "4a1691fd44116c5f8c99a494e6966d73",
+    "url": "/escape-game/static/js/2.7171896b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c8668b533ce46879d3",
+    "url": "/escape-game/static/js/main.8a02d3cb.chunk.js"
+  },
+  {
+    "revision": "ba710ceb8a3a7accb445",
+    "url": "/escape-game/static/js/runtime-main.feb4eb44.js"
+  }
+]);
