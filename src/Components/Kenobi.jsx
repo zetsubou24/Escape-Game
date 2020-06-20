35325a6bc81aa6d7859f8d135866579cf73f6877
Noobie777 <42256429+Noobie777@users.noreply.github.com>
2020-06-20 18:10:50 +0530

    Changed Description

--- src/Components/Kenobi.jsx
+++ src/Components/Kenobi.jsx
@@ -7,17 +7,20 @@ const Kenobi = () => {
 
     console.log("from kenobi",loginExchange.getCredentials())
 
-    const toDis="Astronaut.. can you hear me? Ah. Seems like you're doing okay. Due to some technical error, it seems like you've crash landed. Find your way back to the Home Base. Good luck soldier."
-    
+    // const toDis="Astronaut.. can you hear me? Ah. Seems like you're doing okay. Due to some technical error, it seems like you've crash landed. Find your way back to the Home Base. Good luck soldier."
+    const toDis="Hello Astronauts. Can you hear me?Ah. I can hear you and seems like you're doing fine. Due to alien interception a technicalerror occurred and you've crash landed on MARS. You have everything at your disposal in‘planetary approach suit’. By Demystifying, devise your travel & find your way back to thehuman safe ‘Home Base Pod’. Be cognizant about Oxygen supplyand alien attacks. Yoursurvival is paramount to next human generations. Good luck Astronauts !!"
+    const assign = () => {
+
+    }
     return (
     <div class="container" style={{height:"100vh"}}>
-  <img src="images/bg_welcome2.jpg" className="bg_welcome" alt="bg for breifing page"/>
+  <img src="images/bg_welcome2.jpg" className="bg_welcome"/>
   {/* <div class="kenobi-bottom-left">Please Select your Leader
   </div> */}
   <div class="kenobi-top-left">
-    <p style={{color:"black",marginTop:"30px",fontFamily:"Special Elite"}}>{toDis}</p><br />
-    <div style={{color:"red",fontSize:'40px',marginBottom:"40px"}}>Remember this Soldier:</div>
-    <div style={{color:"red",fontFamily:'Orbitron',fontSize:'48px'}}> {quoteExchange.getRandomQuote()}</div>
+    <p style={{color:"red"}}>{toDis}</p><br />
+    <p style={{color:"red",fontSize:'40px'}}>Remember this Soldier:</p>
+    <p style={{color:"red",fontFamily:'Indie Flower',fontSize:'48px'}}> {quoteExchange.getRandomQuote()}</p>
     {/* <p style={{className:'quote'}}> {quoteExchange.getRandomQuote()}</p> */}
   </div>
   <div class="kenobi-bottom-right">
